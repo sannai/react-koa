18f2d3f55f5d71f9656943b809d7fca72c306430
zzp <965005909@qq.com>
2019-04-28 13:45:13 +0800

    修改样式

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -76,6 +76,17 @@ function Dialog(props) {
             index: 4,
         },
     ]
+    let bodyEl = document.body
+    let top = 0
+    if (props.isMenu) {
+        top = window.scrollY
+        bodyEl.style.position = 'fixed'
+        bodyEl.style.top = -top + 'px'
+    } else {
+        bodyEl.style.position = ''
+        bodyEl.style.top = ''
+        window.scrollTo(0, top) // 回到原先的top
+    }
     return (
         <Container isMenu={props.isMenu}>
             <MyExpand>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,21 +4,29 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { actions } from '../redux'
 
-const Container = styled.ul`
+const Container = styled.main`
     width: 100%;
 `
+const MyUl = styled.ul``
 const MyLi = styled.li`
     margin-bottom: 3vw;
     background-color: #fff;
     box-shadow: 0 0.4vw 0.8vw rgba(26, 26, 26, 0.1);
     padding: 3vw 4.5vw;
     h2 {
-        font-size: 5vw;
+        font-size: 4.35vw;
+        color: #1a1a1a;
     }
+`
+const MyAnalysiHtml = styled.div`
+    margin: 2vw 0;
     img {
         width: 100%;
         height: 30vw;
     }
+`
+const MyAction = styled.div`
+    font-size: 14px;
 `
 
 function Home(props) {
@@ -34,18 +42,20 @@ function Home(props) {
     }, [props])
     return (
         <Container>
-            {props.homeListData.data &&
-                props.homeListData.data.map(item => (
-                    <MyLi key={item._id}>
-                        <h2>{item.title}</h2>
-                        <div dangerouslySetInnerHTML={{ __html: item.introduction }} />
-                        <div>
-                            <span>创建{item.createDate.slice(0, 10)}</span>
-                            <span>评论{item.commentNumber}</span>
-                            <span>阅读{item.readNumber}</span>
-                        </div>
-                    </MyLi>
-                ))}
+            <MyUl>
+                {props.homeListData.data &&
+                    props.homeListData.data.map(item => (
+                        <MyLi key={item._id}>
+                            <h2>{item.title}</h2>
+                            <MyAnalysiHtml dangerouslySetInnerHTML={{ __html: item.introduction }} />
+                            <MyAction>
+                                <span>创建{item.createDate.slice(0, 10)}</span>
+                                <span>评论{item.commentNumber}</span>
+                                <span>阅读{item.readNumber}</span>
+                            </MyAction>
+                        </MyLi>
+                    ))}
+            </MyUl>
         </Container>
     )
 }
